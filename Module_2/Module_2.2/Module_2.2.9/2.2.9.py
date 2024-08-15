2c12ca318ddc345ba7ec36cf70f49411b7d8f401
Garrett <8141816@gmail.com>
2024-08-15 21:24:51 +0300

    solved last task in module 2.2

--- Module_2/Module_2.2/Module_2.2.9/2.2.9.py
+++ Module_2/Module_2.2/Module_2.2.9/2.2.9.py
@@ -54,21 +54,47 @@ Summary: 1 GB
 Примечание 5. Указанный файл доступен по ссылке. Ответ на задачу доступен по ссылке.
 
 Примечание 6. При открытии файла используйте явное указание кодировки UTF-8."""
-import re
+import re, math
 
 def group_size(*args):
     data_group = {}
+    extentions = []
     for el in args:
         data_group[(el[1])] = data_group.get((el[1]), [])  + [[el[0], el[1], el[2], el[3]]]
-    for k, v in da
-    return data_group
+        data_group[(el[1])] = sorted(data_group[(el[1])])
+    for k in data_group.keys():
+        extentions.append(k)
+    extentions = sorted(extentions)
+    for el in extentions:
+        summary = 0
+        for i in range(len(data_group[el])):
+            print(f"{data_group[el][i][0]}.{data_group[el][i][1]}")
+            if data_group[el][i][3] == "GB":
+                summary += int(data_group[el][i][2])*1024*1024*1024
+            elif data_group[el][i][3] == "MB":
+                summary += int(data_group[el][i][2])*1024*1024
+            elif data_group[el][i][3] == "KB":
+                summary += int(data_group[el][i][2])*1024
+            else: 
+                summary += int(data_group[el][i][2])
+        print("----------")
+        if (summary / 1024 < 1):
+            print(f"Summary: {round(summary)} B")
+        elif (summary / 1024 > 1) & (summary / 1024 < 1024):
+            print(f"Summary: {round(summary / 1024)} KB")
+        elif ((summary / (1024*1024))> 1) & ((summary / (1024*1024)) < 1024):
+            print(f"Summary: {round(summary / 1024 / 1024)} MB")
+        elif ((summary / (1024*2024*1024) ) > 1):
+            print(f"Summary: {round(summary / 1024 / 1024 / 1024)} GB")
+        print()
+    return 
 
 
 def read_file():
-    with open("/home/garrett/Documents/GitHub/Stepik_Python_Professional/Module_2/Module_2.2/Module_2.2.9/files.txt", "r") as f:
+    with open("D:\Work\GitHub\Stepik_Python_Professional\Module_2\Module_2.2\Module_2.2.9\/files.txt", "r") as f:
         data_raw = [re.split("[. ]", i.rstrip()) for i in f.readlines()]
     return data_raw
 
 
 
-print(*group_size(*read_file()))
+group_size(*read_file())

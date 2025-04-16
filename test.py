2bf20261b9f699b70bafa3bb7cf46727c7e72913
Garrett <8141816@gmail.com>
2025-04-16 22:36:32 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-def count_occurences(word, words):
-    return Counter(words.lower().split(" "))[word.lower()]
-
-word = 'python'
-words = 'Python Conferences python training python events'
-
-print(count_occurences(word, words))
